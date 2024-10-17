1c2a76f4460fb1560a358012f2d59e8062981af3
Vinícius Mergulhão <113155890+Vinimtt@users.noreply.github.com>
2024-10-17 16:43:42 -0300

    Update MainC.java

--- Clinica/MainC.java
+++ Clinica/MainC.java
@@ -4,30 +4,50 @@ public class MainC {
     public static void main(String[] args) {
         Clinica clinica = new Clinica();
 
-        Paciente paciente = new Paciente("Pablo Felipe",30,"A+");
+        Paciente paciente = new Paciente("Teste 1",20,"B");
         clinica.addPacientes(paciente);
 
-        Paciente paciente2 = new Paciente("Joao Victor",20,"O");
+        Paciente paciente2 = new Paciente("Teste 2",56,"O");
         clinica.addPacientes(paciente2);
 
-        Paciente paciente3 = new Paciente("Vinicius Teti",20,"O");
+        Paciente paciente3 = new Paciente("Teste 3",31,"O");
         clinica.addPacientes(paciente3);
 
-        Medico medico = new Medico("Carlos Aurelio",5425,true);
+        Paciente paciente4 = new Paciente("Teste 3",31,"O");
+        clinica.addPacientes(paciente4);
+
+        Medico medico = new Medico("Doutor teste",1,true);
         clinica.addMedicos(medico);
 
-        Medico medico2 = new Medico("Carla Carina",256458,true);
+        Medico medico2 = new Medico("Doutor teste2",2,true);
         clinica.addMedicos(medico2);
 
-        clinica.addConsulta(paciente, medico, null);
+        Medico medico3 = new Medico("Doutor teste3",3,true);
+        clinica.addMedicos(medico3);
 
-        clinica.addConsulta(paciente2, medico2, null);
+        clinica.addConsulta(paciente, medico, "11/10/2024");
+
+        clinica.addConsulta(paciente2, medico2, "12/10/2024");  
+
+        clinica.addConsulta(paciente3, medico3, "13/10/2024");
 
         clinica.Relatorio();
-
+        
         clinica.searchPaciente(paciente);
+
         clinica.searchPaciente(paciente2);
 
-        clinica.searchTipoSanguineo("A+");
+        clinica.searchPaciente(paciente3);
+
+        clinica.searchTipoSanguineo("O-");
+
+        clinica.searchTipoSanguineo("AB");
+
+        clinica.searchDisponivel(medico);
+
+        clinica.searchDisponivel(medico3);
+
+        clinica.searchDisponivel(medico2);
+
     } 
 }
